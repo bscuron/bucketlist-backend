d72a964267123aed63ea42d5cf6f9ebd44760f40
Ben Scuron <bscuron19@gmail.com>
2023-03-15 15:26:56 -0400

    Require JWT validation by default. Whitelist certain routes
    By default, routes will require a valid JWT to be accessed. Routes can
    be whitelisted (allowed to be accessesd without a token). Whitelisted
    routes are needed for pages such as the signup page and login page.

--- src/modules/express.ts
+++ src/modules/express.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
+import { expressjwt } from 'express-jwt';
 
 // Create express application
 const app: Express = express();
@@ -11,6 +12,27 @@ app.use(cors());
 // Parse the incoming requests with JSON payloads
 app.use(express.json());
 
+// Validate JWTs, except for on public paths
+app.use(
+    expressjwt({
+        secret: process.env.JWT_SECRET,
+        algorithms: ['HS256']
+    }).unless({
+        path: [
+            '/signup',
+            '/login',
+            '/database', // TODO: remove this route when login page is finished
+            /^\/database\/user\/[^/]+$/, // NOTE: needed for real-time form validation
+            /^\/database\/email\/[^/]+$/ // NOTE: needed for real-time form validation
+        ]
+    })
+);
+
+/**
+ * Create JSON Web Token (JWT) with secret
+ *
+ * @returns {string} JSON Web Token
+ */
 const createAuthToken = (payload: object): string => {
     return jwt.sign(payload, process.env.JWT_SECRET);
 };

6b71dfe5f6727370c6b32a38f7caf148e13a6495
Ben Scuron <bscuron19@gmail.com>
2023-04-04 17:58:22 -0400

    Add route to delete event by `event_id`

--- src/index.ts
+++ src/index.ts
@@ -228,6 +228,16 @@ app.delete('/delete', async (req: Request, res: Response) => {
     }
 });
 
+// DELETE route to delete event with `event_id`
+app.delete('/delete/event/:event_id', async (req: Request, res: Response) => {
+    try {
+        await db('events').where('event_id', req.params.event_id).del();
+        res.sendStatus(200); // 200 OK
+    } catch (_) {
+        res.sendStatus(500); // 500 Internal Service Error
+    }
+});
+
 // Start the express server on port `process.env.PORT`
 app.listen(process.env.PORT, () => {
     console.log(`LOG: Server is running on port ${process.env.PORT}`);
